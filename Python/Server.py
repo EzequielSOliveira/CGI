3495daab9cb462d4623446e38c58fd1298bf513b
Ezequiel <ezequieloliveirapessoal@outlook.com>
2022-08-26 17:34:12 -0500

    FEATURE: Added Python CGI

---
+++ Python/Server.py
@@ -0,0 +1,27 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+hostName = "localhost"
+serverPort = 80#9002
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        from service.console import output
+
+        self.wfile.write(bytes(json.dumps(json.loads(output)), "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
